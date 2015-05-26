3d5c0cdad47d32c1540cff2750a2eba96aca3f68
Yifu Yu <root@jackyyf.com>
2015-05-27 03:57:40 +0800

    First release, test & bechmark added.

---
+++ subnets.go
@@ -0,0 +1,171 @@
+package subnets
+
+import (
+	"net"
+)
+
+type bitset []byte
+type elem struct {
+	val  *MatchNode
+	next *elem
+}
+
+type stack struct {
+	*elem
+}
+
+func newStack() *stack {
+	return &stack{
+		elem: new(elem),
+	}
+}
+
+func (s *stack) Push(val *MatchNode) {
+	s.elem = &elem{val, s.elem}
+}
+
+func (s *stack) Pop() *MatchNode {
+	var val *MatchNode
+	val, s.elem = s.elem.val, s.elem.next
+	return val
+}
+
+func (s bitset) Get(idx int) int {
+	bdx := idx >> 3
+	bmask := byte(128 >> uint(idx&7))
+	if s[bdx]&bmask != 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+type MatchNode struct {
+	Full  bool
+	Child [2]*MatchNode
+}
+
+type Matcher struct {
+	limit int
+	root  *MatchNode
+}
+
+type IPv4Matcher struct {
+	Matcher
+}
+
+type IPv6Matcher struct {
+	Matcher
+}
+
+func Newv4Matcher() *IPv4Matcher {
+	return &IPv4Matcher{
+		Matcher: Matcher{
+			limit: 32,
+			root:  new(MatchNode),
+		},
+	}
+}
+
+func Newv6Matcher() *IPv6Matcher {
+	return &IPv6Matcher{
+		Matcher: Matcher{
+			limit: 128,
+			root:  new(MatchNode),
+		},
+	}
+}
+
+func (me *Matcher) Match(ip bitset) bool {
+	now := me.root
+	for idx := 0; idx < me.limit; idx++ {
+		if now.Full {
+			return true
+		}
+		next := ip.Get(idx)
+		if now.Child[next] == nil {
+			return false
+		}
+		now = now.Child[next]
+	}
+	return false
+}
+
+func (me *Matcher) Add(ip bitset, plen int) {
+	now := me.root
+	// Go down to add entry.
+	s := newStack()
+	for idx := 0; idx < plen; idx++ {
+		if now.Full {
+			// Do not go further, since the subnet is covered.
+			return
+		}
+		s.Push(now)
+		next := ip.Get(idx)
+		if now.Child[next] == nil {
+			now.Child[next] = new(MatchNode)
+		}
+		now = now.Child[next]
+	}
+	now.Full = true
+	for idx := 0; idx < plen; idx++ {
+		now = s.Pop()
+		if (now.Child[0] != nil && now.Child[0].Full) &&
+			(now.Child[1] != nil && now.Child[1].Full) {
+			now.Full = true
+			now.Child[0], now.Child[1] = nil, nil
+		} else {
+			return
+		}
+	}
+}
+
+func (me *IPv4Matcher) Match(ipv4 net.IP) bool {
+	if len(ipv4) != net.IPv4len {
+		return false
+	}
+	return me.Matcher.Match(bitset(ipv4))
+}
+
+func (me *IPv4Matcher) Add(ipv4 net.IP, plen int) {
+	if len(ipv4) != net.IPv4len || plen > net.IPv4len {
+		return
+	}
+	me.Matcher.Add(bitset(ipv4), plen)
+}
+
+func (me *IPv6Matcher) Match(ipv6 net.IP) bool {
+	if len(ipv6) != net.IPv6len {
+		return false
+	}
+	return me.Matcher.Match(bitset(ipv6))
+}
+
+func (me *IPv6Matcher) Add(ipv6 net.IP, plen int) {
+	if len(ipv6) != net.IPv6len || plen > net.IPv6len {
+		return
+	}
+	me.Matcher.Add(bitset(ipv6), plen)
+}
+
+func (me *IPv4Matcher) AddNet(ipnet *net.IPNet) {
+	if ipnet == nil {
+		return
+	}
+	if ones, bits := ipnet.Mask.Size(); len(ipnet.IP) != net.IPv4len || (ones == 0 && bits == 0) || bits != net.IPv4len<<3 {
+		return
+	} else {
+		me.Matcher.Add(bitset(ipnet.IP), ones)
+	}
+}
+
+func (me *IPv6Matcher) AddNet(ipnet *net.IPNet) {
+	if ipnet == nil {
+		return
+	}
+	if ones, bits := ipnet.Mask.Size(); len(ipnet.IP) != net.IPv6len || (ones == 0 && bits == 0) || bits != net.IPv6len<<3 {
+		return
+	} else {
+		me.Matcher.Add(bitset(ipnet.IP), ones)
+	}
+}
---
+++ subnets_test.go
@@ -0,0 +1,233 @@
+package subnets
+
+import (
+	// "fmt"
+	"bufio"
+	"bytes"
+	//"io"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"testing"
+)
+
+const RandSize = 32768
+
+func getChnRoute(b *testing.B) (ret *IPv4Matcher) {
+	ret = Newv4Matcher()
+	chnroute, err := ioutil.ReadFile("testdata/chnroute.txt")
+	if err != nil {
+		b.Error("No testdata: chnroute.txt")
+	}
+	reader := bufio.NewReader(bytes.NewReader(chnroute))
+	b.StartTimer()
+	for line, err := reader.ReadBytes('\n'); err == nil; line, err = reader.ReadBytes('\n') {
+		ret.AddNet(parseIPNet(string(line)))
+	}
+	b.StopTimer()
+	return
+}
+
+func getChnRoute6(b *testing.B) (ret *IPv6Matcher) {
+	ret = Newv6Matcher()
+	chnroute, err := ioutil.ReadFile("testdata/chnroute-v6.txt")
+	if err != nil {
+		b.Error("No testdata: chnroute-v6.txt")
+	}
+	reader := bufio.NewReader(bytes.NewReader(chnroute))
+	b.StartTimer()
+	for line, err := reader.ReadBytes('\n'); err == nil; line, err = reader.ReadBytes('\n') {
+		ret.AddNet(parseIPNet(string(line)))
+	}
+	b.StopTimer()
+	return
+}
+
+func parseIPNet(cidr string) (n *net.IPNet) {
+	_, n, _ = net.ParseCIDR(cidr)
+	return
+}
+
+func TestIPv4Basic(t *testing.T) {
+	m := Newv4Matcher()
+	subnets := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+	}
+	for _, subnet := range subnets {
+		m.AddNet(parseIPNet(subnet))
+	}
+	t.Log("Try 192.168.0.1 ...")
+	if res := m.Match(net.IPv4(192, 168, 0, 1).To4()); !res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 172.23.34.45 ...")
+	if res := m.Match(net.IPv4(172, 23, 34, 45).To4()); !res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 10.9.8.7 ...")
+	if res := m.Match(net.IPv4(10, 9, 8, 7).To4()); !res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 8.8.8.8 ...")
+	if res := m.Match(net.IPv4(8, 8, 8, 8).To4()); res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 1.2.4.8 ...")
+	if res := m.Match(net.IPv4(1, 2, 4, 8).To4()); res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+}
+
+func TestIPv4Compress(t *testing.T) {
+	m := Newv4Matcher()
+	subnets := []string{
+		"0.0.0.0/1",
+		"128.0.0.0/2",
+		"192.0.0.0/2",
+	}
+	for _, subnet := range subnets {
+		m.AddNet(parseIPNet(subnet))
+	}
+	t.Log("Check compress status...")
+	if !m.root.Full {
+		t.Error("Error: root is not marked as full!")
+	}
+	if m.root.Child[0] != nil {
+		t.Error("Error: left child is not deleted!")
+	}
+	if m.root.Child[1] != nil {
+		t.Error("Error: right child is not deleted!")
+	}
+	if !t.Failed() {
+		t.Log("OK")
+	}
+}
+
+func TestIPv6Basic(t *testing.T) {
+	m := Newv6Matcher()
+	subnets := []string{
+		"2001:db8::/64",
+		"2001:db8:8000::/48",
+		"2001:db8:8001::/48",
+	}
+	for _, subnet := range subnets {
+		m.AddNet(parseIPNet(subnet))
+	}
+	t.Log("Try 2001:db8::bad:face:f00d:beef ...")
+	if res := m.Match(net.ParseIP("2001:db8::bad:face:f00d:beef")); !res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 2001:db8:8000:c0de:bad:face:f00d:beef ...")
+	if res := m.Match(net.ParseIP("2001:db8:8000:c0de:bad:face:f00d:beef")); !res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 2001:db8:8001:dead:bad:face:f00d:beef ...")
+	if res := m.Match(net.ParseIP("2001:db8:8001:dead:bad:face:f00d:beef")); !res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try fe80::1 ...")
+	if res := m.Match(net.ParseIP("fe80::1")); res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+	t.Log("Try 2001:db8:: ...")
+	if res := m.Match(net.ParseIP("2001:db8:233::")); res {
+		t.Error("Failed!")
+	} else {
+		t.Log("OK")
+	}
+}
+
+func TestIPv6Compress(t *testing.T) {
+	m := Newv6Matcher()
+	subnets := []string{
+		"::/1",
+		"8000::/2",
+		"c000::/3",
+		"e000::/4",
+		"f000::/4",
+	}
+	for _, subnet := range subnets {
+		m.AddNet(parseIPNet(subnet))
+	}
+	t.Log("Check compress status...")
+	if !m.root.Full {
+		t.Error("Error: root is not marked as full!")
+	}
+	if m.root.Child[0] != nil {
+		t.Error("Error: left child is not deleted!")
+	}
+	if m.root.Child[1] != nil {
+		t.Error("Error: right child is not deleted!")
+	}
+	if !t.Failed() {
+		t.Log("OK")
+	}
+}
+
+func randomV4() net.IP {
+	data := make([]byte, 4)
+	for i := 0; i < 4; i++ {
+		data[i] = byte(rand.Int31n(256))
+	}
+	return net.IP(data)
+}
+
+func randomV6() net.IP {
+	data := make([]byte, 16)
+	for i := 0; i < 16; i++ {
+		data[i] = byte(rand.Int31n(256))
+	}
+	return net.IP(data)
+}
+
+func BenchmarkChnRoute(b *testing.B) {
+	test_ip4s := make([]net.IP, RandSize)
+	for i := 0; i < RandSize; i++ {
+		test_ip4s[i] = randomV4()
+	}
+	b.ResetTimer()
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		r := getChnRoute(b)
+		for _, ip4 := range test_ip4s {
+			r.Match(ip4)
+		}
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkChnRoute6(b *testing.B) {
+	test_ip6s := make([]net.IP, RandSize)
+	for i := 0; i < RandSize; i++ {
+		test_ip6s[i] = randomV6()
+	}
+	b.ResetTimer()
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		r := getChnRoute6(b)
+		for _, ip6 := range test_ip6s {
+			r.Match(ip6)
+		}
+	}
+	b.ReportAllocs()
+}
